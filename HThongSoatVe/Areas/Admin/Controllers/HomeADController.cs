c18e9acda6fc536daea946e7a0a5f933708e8893
HDann Day <HDann Day@HDANN>
2023-05-08 20:17:54 +0700

    a

--- HThongSoatVe/Areas/Admin/Controllers/HomeADController.cs
+++ HThongSoatVe/Areas/Admin/Controllers/HomeADController.cs
@@ -13,5 +13,10 @@ namespace HThongSoatVe.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
