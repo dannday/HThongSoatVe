8d334a47bea94bf891a7a6ec74778be83d927ffe
HDann Day <HDann Day@HDANN>
2023-05-15 18:28:06 +0700

    15.5

---
+++ HThongSoatVe/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using HThongSoatVe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HThongSoatVe.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Login
+
+        DBContext db = new DBContext();
+
+        public ActionResult Index()
+        {
+            if (Session["TKAdmin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            string user = collection["form-username"];
+            string password = collection["form-password"];
+
+            NhanVien nv = db.NhanViens.SingleOrDefault(a => a.sdt == user && a.pass == password);
+            if (nv == null)
+            {
+                ViewBag.ThongBaoAdmin = "Tài Khoản Hoặc Mật Khẩu Sai";
+                return this.Login();
+            }
+            Session["TKAdmin"] = nv;
+            return RedirectToAction("Index", "Admin");
+        }
+
+    }
+}
