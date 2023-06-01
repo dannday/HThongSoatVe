0c4a539af2c1017f576e967ecf8f2e226c042af6
HDann Day <HDann Day@HDANN>
2023-06-01 22:35:53 +0700

    1.6

---
+++ HThongSoatVe/Controllers/ChuongTrinhController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HThongSoatVe.Controllers
+{
+    public class ChuongTrinhController : Controller
+    {
+        // GET: ChuongTrinh
+        public ActionResult TieuDiem()
+        {
+            return View();
+        }
+
+        public ActionResult CongDong()
+        {
+            return View();
+        }
+
+        public ActionResult NgheSi()
+        {
+            return View();
+        }
+
+        public ActionResult LichDien()
+        {
+            return View();
+        }
+    }
+}
